aa6d7d09b6dd0a4c3964f80f824c0ce820d34566
GirishGautam5 <girishkumarnarreddy@gmail.com>
2022-05-08 12:57:32 +0530

    Added Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,31 @@
 import React from "react";
-import data from "./data";
-import Product from "./components/Product";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProductScreen from "./Screens/ProductScreen";
+import HomeScreen from "./Screens/HomeScreen";
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            Fashion World
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">SignIn</a>
-        </div>
-      </header>
-      <main>
-        <div>
-          <div className="row center">
-            {data.products.map((product) => (
-             <Product key={product._id} product={product}/>
-            ))}
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              Fashion World
+            </a>
           </div>
-        </div>
-      </main>
-      <footer className="row center">All Rights Reserved</footer>
-    </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">SignIn</a>
+          </div>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" exact element={<HomeScreen />}></Route>
+            <Route path="/product/:id" element={<ProductScreen />}></Route>
+          </Routes>
+        </main>
+        <footer className="row center">All Rights Reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
