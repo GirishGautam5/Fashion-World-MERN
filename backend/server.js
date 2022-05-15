a70f9fb5b49c7ffcb6edf39ed245e555f90ed892
GirishGautam5 <girishkumarnarreddy@gmail.com>
2022-05-15 22:41:44 +0530

    made connection to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 import express from 'express'
 import data from './data.js'
+import mongoose from 'mongoose'
+import userRouter from './routers/userRouter.js'
 const app = express()
+
+mongoose.connect('mongodb://localhost/fashionstore',{
+  
+})
 
 app.get('/',(req,res)=> {
     res.send("Server is Running")
@@ -13,9 +19,11 @@ app.get('/api/products/:id',(req,res)=>{
         res.status(404).send({message: 'Product not found'})
     }
 })
+
 app.get('/api/products',(req,res)=>{
     res.send(data.products)
 })
+app.use('/api/users',userRouter)
 
 const port = process.env.PORT || 5000;
 app.listen(port, ()=> {
